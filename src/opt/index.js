b87d6fca7647556053fbe13baca49b4a9526eeec
Maikel Sleebos <maikel@egotribe.nl>
2022-05-15 17:34:46 +0200

    added onboarding

--- src/opt/index.js
+++ src/opt/index.js
@@ -10,25 +10,73 @@
     const videoContainer = document.querySelector('.video-personal')
     const videoContainers = document.querySelectorAll('.video-student:not(.video-personal)')
     const myVideo = document.createElement('video')
+    const popupEl = document.querySelector('.popup')
 
     let myVideoStream
 
     myVideo.muted = true
     myVideo.classList.add('user')
 
+    const popup = {
+        init:()=>{
+            popupEl.querySelector('.popup__close').addEventListener('click',popup.closePopup)
+            popupEl.querySelector('.popup__next span:last-child').addEventListener('click',popup.closePopup)
+            popupEl.querySelector('.popup__prev').addEventListener('click',popup.prev)
+            popupEl.querySelector('.popup__next').addEventListener('click',popup.next)
+        },
+        closePopup: ()=>{
+            popupEl.classList.remove('active')
+        },
+        prev:()=>{
+            const activeSlide = popupEl.querySelector('.popup__slide.active')
+            const activeSlideIndicator = popupEl.querySelector('.popup__slide-ball.active')
+            if(activeSlide.previousElementSibling){
+                activeSlide.previousElementSibling.classList.add('active')
+                activeSlide.classList.remove('active')
+                activeSlideIndicator.previousElementSibling.classList.add('active')
+                activeSlideIndicator.classList.remove('active')
+            }
+            popupEl.querySelector('.popup__next span:first-child').classList.add('active')
+            popupEl.querySelector('.popup__next span:last-child').classList.remove('active')
+        },
+        next:()=>{
+            const activeSlide = popupEl.querySelector('.popup__slide.active')
+            const activeSlideIndicator = popupEl.querySelector('.popup__slide-ball.active')
+            if(activeSlide.nextElementSibling){
+                activeSlide.nextElementSibling.classList.add('active')
+                activeSlide.classList.remove('active')
+                activeSlideIndicator.nextElementSibling.classList.add('active')
+                activeSlideIndicator.classList.remove('active')
+                if(activeSlide.nextElementSibling.nextElementSibling){
+                    popupEl.querySelector('.popup__next span:first-child').classList.add('active')
+                    popupEl.querySelector('.popup__next span:last-child').classList.remove('active')
+                }else{
+                    popupEl.querySelector('.popup__next span:first-child').classList.remove('active')
+                    popupEl.querySelector('.popup__next span:last-child').classList.add('active')
+                }
+            }
+        },
+    }
     const app = {
         init: () => {
             if (app.check()) {
                 app.initMedia()
             }
+            popup.init()
         },
         check: () => {
+            if(!localStorage.getItem('tafelen-thisUser')){
+                localStorage.setItem('tafelen-thisUser',prompt('Wat is je naam') )  
+                popupEl.classList.add('active')
+            }else{
+                console.log(localStorage.getItem('tafelen-thisUser'))
+            }
             if (videoContainer) {
                 console.log('classroom')
-                return true
+                return true;
             } else {
                 console.log('classroomn\'t')
-                return false
+                return false;
             }
         },
         initMedia: () => {
